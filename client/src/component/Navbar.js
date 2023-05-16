c9c7c9267da89677b06e22244512526d22958d67
Hari-haran-p <harichris28@gmail.com>
2023-05-16 14:30:49 +0530

    responsiveness

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,30 +1,52 @@
-import '../css/Navbar.css';
+import { useEffect, useRef, useState } from "react";
+import "../css/Navbar.css";
 
-function Navbar(){
-    return(
-        <div className="navbar-container" >
-            <div className="left-nav" >
-                Corsera
-            </div>
-                <ul className="nav-list">
-                    <li className="nav-item">Home</li>
-                    <li className="nav-item">Product</li>
-                    <li className="nav-item">pricing</li>
-                    <li className="nav-item">contact</li>
-                </ul>
-                <div className='burger' >
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
-            
-                <ul>
-                    <li>home</li>
-                    <li>home</li>
-                    <li>home</li>
-                    <li>home</li>
-                </ul>
-        </div>
-    )
+function Navbar() {
+
+    const navref = useRef(null)
+
+const toggle = () => {
+    if(navref.current.classList == ""){
+
+      navref.current.classList.add("showmenu");}
+      else{
+        navref.current.classList.remove("showmenu");
+      }
 }
-export default Navbar
+
+
+
+
+  return (
+    <div className="body">
+      <nav>
+        <div class="logo">
+          <a href="">Matrix</a>
+        </div>
+
+        <ul className="" ref={navref}>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+
+
+        <div class="menu" onClick={toggle}>
+          <div class="menu-line"></div>
+          <div class="menu-line"></div>
+          <div class="menu-line"></div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
