9763f0c6622686e8d4ac620f8c6a4221111852a0
PrashanthSai-K <114280375+PrashanthSai-K@users.noreply.github.com>
2023-05-19 11:34:34 +0530

    Db connected

--- server/server.js
+++ server/server.js
@@ -13,25 +13,28 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
 const connectionParam = {useNewUrlParser:true,useUnifiedTopology:true}
-const uri = "mongodb+srv://root:abcd1234@demo.vojbrj9.mongodb.net/demo?retryWrites=true&w=majority"
+const uri =
+  "mongodb+srv://root:abcd1234@anime.tpuvkvi.mongodb.net/anime?retryWrites=true&w=majority";
 const connection = mongoose.connect(uri,connectionParam)
                             .then(()=>console.log("MongoDB Connected Successfully"))
                             .catch((error)=>console.log(error))
 
 const userSchema = new mongoose.Schema({
     name:'string',
-    rollno:"string",
-    dept:"string"
+    desc:"string",
+    image:"string"
 })
 
-const user = mongoose.model('user',userSchema,"user")
+const user = mongoose.model('course',userSchema)
 
 app.get("/api/",(req,res)=>{
-    user.find({})
+
+    user.find()
         .then((users)=>res.send(users))
         .catch((error)=>console.log(error))
 })
+
     
 module.exports.server = sls(app);
 
-// app.listen(5000,()=>console.log("app running on localhost 5000"))
+app.listen(5000,()=>console.log("app running on localhost 5000"))
