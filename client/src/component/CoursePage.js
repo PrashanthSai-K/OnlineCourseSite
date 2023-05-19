d618420b87348a43e4ffdf23a484ecd9b3292299
PrashanthSai-K <114280375+PrashanthSai-K@users.noreply.github.com>
2023-05-19 11:31:39 +0530

    pull request

--- client/src/component/CoursePage.js
+++ client/src/component/CoursePage.js
@@ -1,64 +1,47 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../css/CoursePage.css";
 
+import axios from "axios";
+
 export default function CoursePage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchdata() {
+      const response = await axios("http://localhost:5000/api/");
+      setData(response.data);
+    }
+    fetchdata();
+    console.log(data);
+  });
+
   return (
     <>
       <center className="course-heading">Online Course</center>
       <div className="course-container">
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div
-              className="flip-card-front"
-              style={{
-                background: "url('./images/img1.png')",
-                backgroundSize: "100%",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-            <div className="flip-card-back">
-              <p className="title">BACK</p>
-              <p>Leave Me</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div
-              className="flip-card-front"
-              style={{
-                background: "url('./images/img1.png')",
-                backgroundSize: "100%",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-            <div className="flip-card-back">
-              <p className="title">BACK</p>
-              <p>Leave Me</p>
+        {data.map((datas) => {
+          return (
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div
+                  className="flip-card-front"
+                  style={{
+                    background:   `url(${datas.image})`,
+                    backgroundSize: "100%",
+                    backgroundSize: "cover",
+                    backgroundPosition:"center",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
+                <div className="flip-card-back">
+                  <p className="title">{datas.name}</p>
+                  <p>{datas.desc}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div
-              className="flip-card-front"
-              style={{
-                background: "url('./images/img1.png')",
-                backgroundSize: "100%",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-            <div className="flip-card-back">
-              <p className="title">BACK</p>
-              <p>Leave Me</p>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
